44b4a0c94ea3e4ea638c00eb09c46b847065969a
Alireza Forghani Toosi <alirezaft98@gmail.com>
2025-12-02 16:37:56 +0330

    Add some missing files

---
+++ Assets/Runtime/AssemblyVisibility.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("UnityChart.Editor")]
+[assembly:InternalsVisibleTo("UnityChart.Tests.Runtime")]
+[assembly:InternalsVisibleTo("UnityChart.Tests.Editor")]
