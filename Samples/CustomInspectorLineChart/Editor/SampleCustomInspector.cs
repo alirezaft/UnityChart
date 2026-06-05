b5cff5f1d0005d41fea085be6f1e43745192c537
Alireza Forghani Toosi <alirezaft98@gmail.com>
2026-06-05 17:00:14 +0330

    Fix bugs in custom inspector sample

--- Samples/CustomInspectorLineChart/Editor/SampleCustomInspector.cs
+++ Samples/CustomInspectorLineChart/Editor/SampleCustomInspector.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 [CustomEditor(typeof(SampleDataGenerator)), CanEditMultipleObjects]
-public class SampleEditorWindow : Editor
+public class SampleCustomInspector : Editor
 {
-    [SerializeField] private VisualTreeAsset uxmlFile;
+    private string uxmlFileGUID = "76a836259d1452f4e92e8bc11e30111e";
 
     public override VisualElement CreateInspectorGUI()
     {
         VisualElement root = new VisualElement();
+
+        var assetPath = AssetDatabase.GUIDToAssetPath(uxmlFileGUID);
+        var uxmlFile = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(assetPath); 
+            
         uxmlFile.CloneTree(root);
         root.Q<LineChart>().Owner = new DataProviderOwner((SampleDataGenerator)target);
-
+        
         return root;
     }
 }
